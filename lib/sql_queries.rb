df93200a5fa3ec7b09aaa463238198eebc271602
AT Williams <aderiannaw@yahoo.com>
2018-08-02 02:46:48 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,11 +15,11 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  ""
+  "SELECT MIN(age), name from bears;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  " SELECT MAX(color) count FROM bears;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
